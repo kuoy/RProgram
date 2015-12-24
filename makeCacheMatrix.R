9f8b91a04299e53dd5c04addd6b94922e2298db8
kuoy <kuoy@users.noreply.github.com>
2015-12-24 18:33:18 -0500

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,7 +1,11 @@
 # R Programing Assignment 2
-## The makeCacheMatrix function supports users to seek an inverse matrix in the Global Environment. Its input is a valid matrix that assumes its inverse matrix exists. ##
+## The makeCacheMatrix function contains the information of a matrix and its existing 
+## inverse matrix in a global environment.  The makeCacheMatrix function contains of four ## children functions in a list format, explained below: 
 
-## descriptions about functions##
+## (1) the set function stores the data of either its original matrix or a new assigned matrix. Its inverse matrix is set as NULL because no inverse matrix attained yet.
+## (2) the get function displays the stored matrix data
+## (3) the setinvmatrix function stores the data of the inverse matrix m from the cacheSolve function  
+## (4) the getinvmatrix function displays the data of the stored inverse matrix m ##
 
 makeCacheMatrix<- function(x = matrix()) {
   m <- NULL
