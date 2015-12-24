640812877ec5338db8fb8f04d3b9b3fc18d88113
kuoy <kuoy@users.noreply.github.com>
2015-12-24 18:37:41 -0500

    Update cacheSolve.R

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,3 +1,10 @@
+## The cacheSolve function responds to the request of an inverse matrix.  There are two 
+## steps in this function:
+## (1) verify the value of m that was stored in the given variable and saved in the getinvmatrix function.If the value of m saved in the memory is not NULL,   
+## which indicates that the matrix has an existing inverse matrix saved; then this cacheSolve function returns a message “getting cached data” and does nothing. 
+## (2) if the value of m is NULL, it means that this is the first time of the request for getting the inverse matrix. 
+## Then, the matrix data is extracted from the $get() and its inverse matrix is returned and stored at $setinvmatrix(). ##
+
 cacheSolve <- function(x, ...) {
   m <- x$getinvmatrix()
   if(!is.null(m)) {
